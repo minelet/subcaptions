40d7d721cefa97594c0fe980c90b0e0476504556
minelet <ekanshmcdelet@gmail.com>
2026-06-16 20:48:58 +0530

    Add files via upload

---
+++ subcaption-v3-updated_2/lib/packs.js
@@ -0,0 +1,9 @@
+const PACKS = {
+  starter: { credits: 10,   priceUsd: 2   },
+  popular: { credits: 35,   priceUsd: 5   },
+  pro:     { credits: 100,  priceUsd: 10  },
+  bulk:    { credits: 250,  priceUsd: 20  },
+  godly:   { credits: 2000, priceUsd: 100 },
+};
+
+module.exports = { PACKS };
